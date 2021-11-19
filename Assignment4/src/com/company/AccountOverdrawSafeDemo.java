0c2a28f669fa0dd370c380a9389e3f771d20ad47
Shubham <shubham.chaursiya@hotwaxsystems.com>
2021-11-19 23:47:45 +0530

    Resolve overlapping problem

--- Assignment4/src/com/company/AccountOverdrawSafeDemo.java
+++ Assignment4/src/com/company/AccountOverdrawSafeDemo.java
@@ -1,2 +1,34 @@
-package com.company;public class AccountOverdrawSafeDemo {
+package com.company;
+
+public class AccountOverdrawSafeDemo extends Thread{
+    double amount;
+    AccountOverdrawSafeDemo(double amount)
+    {
+        this.amount =amount;
+    }
+
+
+    public double getAmount() {
+        return amount;
+    }
+
+    synchronized void withdraw1 (double withdrawAmt){
+        Thread t=Thread.currentThread();
+        if(getAmount()>= withdrawAmt){
+            System.out.println("Before Withdraw Account balance of "+t.getName()+" is "+getAmount());
+            amount=getAmount()-withdrawAmt;
+            System.out.println(withdrawAmt+"Amount debited by "+t.getName()+" and current amount is "+getAmount());
+
+        }else{
+            System.out.println("Not enough balance for "+t.getName());
+        }
+    }
+
+
+}
+class SynchronizeDemo extends Thread {
+    AccountOverdrawSafeDemo a1 = new AccountOverdrawSafeDemo(1000.00);
+    public void run() {
+        a1.withdraw1(500.0);
+    }
 }
