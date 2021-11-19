9558228f2a223e8929926b96e9a2fc2687ba878a
Shubham <shubham.chaursiya@hotwaxsystems.com>
2021-11-20 00:20:14 +0530

    All about student information

--- Assignment5/src/com/company/Student.java
+++ Assignment5/src/com/company/Student.java
@@ -1,2 +1,37 @@
-package com.company;public class Student {
+package com.company;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String firstName;
+    private String dateOfBirth;
+    public Student(String firstName,String dateOfBirth)
+    {
+        this.firstName=firstName;
+        this.dateOfBirth=dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
 }
